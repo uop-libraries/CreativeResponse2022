bafa99bc32ab85be7b70c3fe51e87f1a5ec6772b
Prot0ast <a_barragan1@u.pacific.edu>
2022-08-23 11:07:55 -0700

    Updating current progress with audio

--- Fellowship2022/Assets/Scripts/ContinueAudio.cs
+++ Fellowship2022/Assets/Scripts/ContinueAudio.cs
@@ -1,44 +1,73 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class ContinueAudio : MonoBehaviour
 {
    public string tagSpecification;
-   private AudioSource audio;
+   private AudioSource boombox;
    private AudioClip currentBop;
 
    private void Awake()
-    {
+    { 
         switch (tagSpecification)
        {
            case "Meet the Students":
-               audio = this.GetComponent<AudioSource>();
+               boombox = this.GetComponent<AudioSource>();
                currentBop = Resources.Load("Bops/Meet the Students Music") as AudioClip;
-               audio.clip = currentBop;
+               boombox.clip = currentBop;
                break;
            case "Assembly Center":
-               audio = this.GetComponent<AudioSource>();
+               boombox = this.GetComponent<AudioSource>();
                currentBop = Resources.Load("Bops/Relocation Music") as AudioClip;
-               audio.clip = currentBop;
+               boombox.clip = currentBop;
                break;
            case "War is Over":
-               audio = this.GetComponent<AudioSource>();
+               boombox = this.GetComponent<AudioSource>();
                currentBop = Resources.Load("Bops/Redress Music") as AudioClip;
-               audio.clip = currentBop;
+               boombox.clip = currentBop;
                break;
        }
+        DontDestroyOnLoad(this.gameObject);
     }
 
    private void Start()
     {
-        audio.Play();
+        boombox.Play();
     }
 
-   private void WhatPlayin()
+   private void ChangeBop()
    {
-       if (tagSpecification == "Meet the Students")
+       if (GameObject.FindGameObjectWithTag("Meet the Students"))
        {
+           tagSpecification = "Meet the Students";
+           boombox = this.GetComponent<AudioSource>();
+           currentBop = Resources.Load("Bops/Meet the Students Music") as AudioClip;
+           Debug.Log("Now playing: " + currentBop);
+           boombox.clip = currentBop;
        }
+       else if (GameObject.FindGameObjectWithTag("Assembly Center"))
+       {
+           tagSpecification = "Assembly Center";
+           boombox = this.GetComponent<AudioSource>();
+           currentBop = Resources.Load("Bops/Meet the Students Music") as AudioClip;
+           Debug.Log("Now playing: " + currentBop);
+           boombox.clip = currentBop;
+       }
+       else if (GameObject.FindGameObjectWithTag("War is Over"))
+       {
+           tagSpecification = "War is Over";
+           boombox = this.GetComponent<AudioSource>();
+           currentBop = Resources.Load("Bops/Meet the Students Music") as AudioClip;
+           Debug.Log("Now playing: " + currentBop);
+           boombox.clip = currentBop;
+       }
+   }
+
+   private void FixedUpdate()
+   {
+       ChangeBop();
    }
 }
