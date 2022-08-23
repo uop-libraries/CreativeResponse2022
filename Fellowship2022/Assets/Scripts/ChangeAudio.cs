a9820dfd89de90509a8ccef99f4b1258eff2b636
Prot0ast <a_barragan1@u.pacific.edu>
2022-08-23 12:43:22 -0700

    LETSSSSS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- Fellowship2022/Assets/Scripts/ChangeAudio.cs
+++ Fellowship2022/Assets/Scripts/ChangeAudio.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class ChangeAudio : MonoBehaviour
 {
+    public AudioClip newTrack;
+    private ContinueAudio theThing;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        theThing = FindObjectOfType<ContinueAudio>();
+        theThing.ChangeBGM(newTrack);
     }
 
     // Update is called once per frame
--- Fellowship2022/Assets/Scripts/ContinueAudio.cs
+++ Fellowship2022/Assets/Scripts/ContinueAudio.cs
@@ -6,34 +6,21 @@ using UnityEngine;
 
 public class ContinueAudio : MonoBehaviour
 {
-   public string tagSpecification;
-   public AudioSource boombox;
-   private AudioClip currentBop;
-
+    public AudioSource boombox;
+   
    private void Awake()
-    { 
-        switch (tagSpecification)
-       {
-           case "Meet the Students":
-               boombox = this.GetComponent<AudioSource>();
-               currentBop = Resources.Load("Bops/Meet the Students Music") as AudioClip;
-               boombox.clip = currentBop;
-               break;
-           case "Assembly Center":
-               boombox = this.GetComponent<AudioSource>();
-               currentBop = Resources.Load("Bops/Relocation Music") as AudioClip;
-               boombox.clip = currentBop;
-               break;
-           case "War is Over":
-               boombox = this.GetComponent<AudioSource>();
-               currentBop = Resources.Load("Bops/Redress Music") as AudioClip;
-               boombox.clip = currentBop;
-               break;
-       }
+    {
         DontDestroyOnLoad(this.gameObject);
     }
    private void Start()
     {
         boombox.Play();
     }
+
+   public void ChangeBGM(AudioClip ssong)
+   {
+       boombox.Stop();
+       boombox.clip = ssong;
+       boombox.Play();
+   }
 }
