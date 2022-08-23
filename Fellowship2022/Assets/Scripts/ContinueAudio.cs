261e8702ba5f7158e1ad2b71fbaf621d1140936c
Prot0ast <a_barragan1@u.pacific.edu>
2022-08-23 09:46:38 -0700

    Renamed audio files to be easier to use, created resources folder to call them via script, and music is capable of being intiated and played via script

--- Fellowship2022/Assets/Scripts/ContinueAudio.cs
+++ Fellowship2022/Assets/Scripts/ContinueAudio.cs
@@ -5,13 +5,40 @@ using UnityEngine;
 public class ContinueAudio : MonoBehaviour
 {
    public string tagSpecification;
+   private AudioSource audio;
+   private AudioClip currentBop;
+
    private void Awake()
+    {
+        switch (tagSpecification)
+       {
+           case "Meet the Students":
+               audio = this.GetComponent<AudioSource>();
+               currentBop = Resources.Load("Bops/Meet the Students Music") as AudioClip;
+               audio.clip = currentBop;
+               break;
+           case "Assembly Center":
+               audio = this.GetComponent<AudioSource>();
+               currentBop = Resources.Load("Bops/Relocation Music") as AudioClip;
+               audio.clip = currentBop;
+               break;
+           case "War is Over":
+               audio = this.GetComponent<AudioSource>();
+               currentBop = Resources.Load("Bops/Redress Music") as AudioClip;
+               audio.clip = currentBop;
+               break;
+       }
+    }
+
+   private void Start()
+    {
+        audio.Play();
+    }
+
+   private void WhatPlayin()
    {
-      GameObject[] musicObject = GameObject.FindGameObjectsWithTag(tagSpecification);
-      if (musicObject.Length > 1)
-      {
-         Destroy(this.gameObject);
-      }
-      DontDestroyOnLoad(this.gameObject);
+       if (tagSpecification == "Meet the Students")
+       {
+       }
    }
 }
