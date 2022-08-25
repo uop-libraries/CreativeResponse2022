cd895bcddc149c99db50d8451baa7a76a44b7cfd
Prot0ast <a_barragan1@u.pacific.edu>
2022-08-24 22:07:49 -0700

    Making tests for muting audio on executive order, removed the deadends in the japanese club before section

---
+++ Fellowship2022/Assets/Scripts/ForcedVolumeChange.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForcedVolumeChange : MonoBehaviour
+{
+    
+    private ContinueAudio theThing;
+    void Awake()
+    {
+        theThing = FindObjectOfType<ContinueAudio>();
+    }
+
+    public void ForceMute()
+    {
+        theThing.AdjustVolume(0);
+    }
+
+    public void ForceUnmute()
+    {
+        theThing.AdjustVolume(1);
+    }
+}
