0cdd4fd7267126ff638087c6ba85db7a5026ac1b
Prot0ast <a_barragan1@u.pacific.edu>
2022-08-25 13:58:06 -0700

    Fixed the giant audio issue, will refine shortly after presentation

--- Fellowship2022/Assets/Scripts/ChangeAudio.cs
+++ Fellowship2022/Assets/Scripts/ChangeAudio.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ChangeAudio : MonoBehaviour
 {
     public AudioClip newTrack;
-    private ContinueAudio theThing;
+    private static ContinueAudio theThing;
     // Start is called before the first frame update
     void Awake()
     {
