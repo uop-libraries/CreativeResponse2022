7beff0035725967c9f4c75d6862c1d1e2cf0b9d1
cfines <c_fines@u.pacific.edu>
2022-08-26 10:43:33 -0700

    yeh comment

--- Fellowship2022/Assets/Scripts/AdjustVolume.cs
+++ Fellowship2022/Assets/Scripts/AdjustVolume.cs
@@ -6,7 +6,7 @@ public class AdjustVolume : MonoBehaviour
 {
     private ContinueAudio theThing;
     public float volumeIntensity;
-
+    // yeh
     //this will change the volume to whatever is directly specified in the enter field on the script
     void Awake()
     {
